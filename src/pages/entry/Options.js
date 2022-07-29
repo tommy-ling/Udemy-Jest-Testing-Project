a731aa0e2211218c664ca5cce67e7f31aa8ff464
Tommy Lin <tommy.lin@rotman.utoronto.ca>
2022-07-29 13:51:49 -0400

    test and code alert flags

--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -3,18 +3,24 @@ import { useEffect, useState } from "react";
 import ScoopOptions from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import { Row } from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItmes] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItmes(response.data))
       .catch((error) => {
-        // TODO: error to catch
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOption;
 
